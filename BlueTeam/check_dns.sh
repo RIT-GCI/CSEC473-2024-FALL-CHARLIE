40b04e91fe86b4cea6f3147def752cc47bd32c22
Nick Kariotis <njk5351@rit.edu>
2024-10-01 16:01:18 -0400

    Check if dns is running, restart if not

---
+++ BlueTeam/check_dns.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# DNS Service
+service="named"
+
+if systemctl is-active --quiet $service; then
+    echo "$service is running"
+else
+    echo "$service is not running, attempting to restart..."
+    systemctl restart $service
+    if systemctl is-active --quiet $service; then
+        echo "$service successfully restarted"
+    else
+        echo "Failed to restart $service"
+    fi
+Fi
